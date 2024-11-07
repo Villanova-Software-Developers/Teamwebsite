c759f123c5308783bb4fb5540ecb736e4d9a17bf
JayBradCoding <138684111+JayBradCoding@users.noreply.github.com>
2024-11-06 21:15:36 -0500

    Adding routes

--- sdc-website/src/App.js
+++ sdc-website/src/App.js
@@ -1,11 +1,17 @@
-
+import { Routes, Route } from 'react-router-dom';
+import {Home, Members, Projects, Faq, Team, SubmitIdea} from './pages/index';
 
 function App() {
   return (
     <div>
-      <header>
-        123
-      </header>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/members" element={<Members/>}/>
+        <Route path="/projects" element={<Projects/>} />
+        <Route path="/faq" element={<Faq/>}/>
+        <Route path="/team" element={<Team/>}/>
+        <Route path="/submit-idea" element={<SubmitIdea/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ sdc-website/src/pages/index.js
@@ -0,0 +1,7 @@
+
+export { default as Home } from './home';
+export { default as Members } from './members';
+export { default as Projects } from './projects';
+export { default as Faq } from './faq-contact';
+export { default as Team } from './team';
+export { default as SubmitIdea } from './submit-idea';
